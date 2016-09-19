086ab75133dfd0722b10ecff2d640e93fc23204a
Leoo34 <leo-ambs@laposte.net>
2016-09-19 11:57:04 +0200

    Exercices.java
    exo java

---
+++ Exercices.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercices;
+import java.util.Scanner; 
+
+/**
+ *
+ * @author etudiant
+ */
+public class Exercices {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        //System.out.println("Hello world");
+        
+        Scanner ac = new Scanner(System.in);
+        
+        /*System.out.println("Veuillez saisir votre nom : ");
+        String str = ac.next();
+        System.out.println ("Bonjour " + str);*/
+        
+        
+        /*int i = 1*2*3;
+        System.out.println("La factorielle de 3 est : " + i);*/
+        
+        
+        /*System.out.println("Veuillez saisir la 1ere note : ");
+        int note1 = ac.nextInt();
+        
+        System.out.println("Veuillez saisir la 2eme note : ");
+        int note2 = ac.nextInt();
+        
+        System.out.println("Veuillez saisir la 3eme note : ");
+        int note3 = ac.nextInt();
+        
+        System.out.println("Moyenne des 3 notes : " + ((note1 + note2 + note3)/3) + "/20");*/
+        
+        
+        /*System.out.println("Veuillez saisir la base du triangle : ");
+        int base = ac.nextInt();
+        
+        System.out.println("Veuillez saisir la hauteur du triangle : ");
+        int haut = ac.nextInt();
+        
+        System.out.println("L'aire du triangle est :  " + (base * haut)/2);*/
+        
+        
+        /*System.out.println("Veuillez saisir le rayon du cercle : ");
+        int rayon = ac.nextInt();
+        
+        System.out.println("L'aire d'une cercle est :  " + (Math.PI * (rayon*rayon)));*/
+        
+    }
+  
+}
