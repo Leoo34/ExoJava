de3f26ba134d726a88ad96e50838f5cf2af7e1c1
Leoo34 <leo-ambs@laposte.net>
2016-09-20 14:03:18 +0200

    Update Exercices.java

--- Exercices.java
+++ Exercices.java
@@ -5,6 +5,7 @@
  */
 package exercices;
 import java.util.Scanner; 
+import java.util.Random;
 
 /**
  *
@@ -17,10 +18,15 @@ public class Exercices {
      */
     public static void main(String[] args) {
        
+        //Exercice 1
+        //Affiche Hello world
         //System.out.println("Hello world");
         
-        Scanner ac = new Scanner(System.in);
+        //declaration d'un scanner pour la suite des exercices
+        //Scanner ac = new Scanner(System.in);
         
+        //Exercice 2
+        //Demande à l'utlisateur de saisir son nom puis affiche un bonjour personalisé avec le nom de l'utilisateur
         /*System.out.println("Veuillez saisir votre nom : ");
         String str = ac.next();
         System.out.println ("Bonjour " + str);*/
@@ -50,12 +56,98 @@ public class Exercices {
         
         System.out.println("L'aire du triangle est :  " + (base * haut)/2);*/
         
-        
+        //Exercice 6
         /*System.out.println("Veuillez saisir le rayon du cercle : ");
         int rayon = ac.nextInt();
         
         System.out.println("L'aire d'une cercle est :  " + (Math.PI * (rayon*rayon)));*/
         
+        
+        //Exercice 7 
+        //Demande...
+        /*System.out.println("Veuillez saisir un chiffre : ");
+        int facto = ac.nextInt();
+        
+        if (facto !=0){
+            for (int i=facto-1 ;i>=1;i--){
+                facto=facto*i;
+            }
+            System.out.println("donc le factoriel est =  "+facto);
+        } else{
+            facto=1;
+            System.out.println("le factoriel est = "+facto);
+        }*/
+        
+        //Exercice 8
+        /*int nombreNotes;
+        System.out.println("Nombre de notes a saisir ?");
+        nombreNotes = ac.nextInt();
+        
+        double [] lesNotes = new double[nombreNotes];
+        
+        for (int i=0; i<= lesNotes.length -1; i++) {
+           System.out.println("Note n°" + (i+1) + " :");
+           lesNotes[i] = ac.nextInt();
+        }
+        
+        double moyenne = 0;
+        
+        for (int i=0; i<= lesNotes.length -1; i++) {
+           moyenne = moyenne + lesNotes[i];
+        }
+        System.out.println("La moyenne des notes est : "+ moyenne/nombreNotes);    
+        */
+        
+        //Exercice 9
+        
+        /*int nombre,essais=0;
+        Random rand = new Random();
+        int nombreAtrouver = rand.nextInt(101);
+
+        System.out.println("-Le juste prix-");
+        System.out.println("--------------------------------------------");
+        System.out.println("Trouvez le nombre mystere entre 0 et 100 !");
+            do{
+           
+            System.out.println("Veuillez entrer un nombre...");
+            nombre=ac.nextInt();       
+
+            if (nombre<nombreAtrouver)
+                {
+                System.out.println("C'est plus ! ");
+                essais++;
+                }
+            else if (nombre >nombreAtrouver)
+                {
+                System.out.println("C'est moins !");
+                essais++;
+                }
+            else
+                {
+                System.out.println("Trouvé en " + essais + " essais ! Bien joué !");
+                }
+            }
+            while(nombre!=nombreAtrouver);*/
+ 
     }
+    
+    //Exercice 10
+    public boolean plusGrand(int a, int b)  {
+        a = 2;
+        b = 3;
+        if (a > b){
+            return true;
+        }else{
+            return false;
+        } 
+    }
+     
+    //Exercice 11
+    public void pythagore(int a, int b){
+        a = 2;
+        b = 3;
+        
+
+    } 
   
 }
